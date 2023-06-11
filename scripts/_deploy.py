15b427de2aba304c46e9a2beb657f2fc55338237
Lingxuan Ye <love@lingxuan.io>
2023-06-12 03:14:04 +0800

    fix: Fix a bug that occurs when a symlink points to a non-existing directory or file

--- scripts/_deploy.py
+++ scripts/_deploy.py
@@ -8,36 +8,46 @@ assert Path.cwd() == Path(__file__).parent.parent
 
 class UnsupportedPath(Exception):
     """
-    Raised when a path is neither a file nor a directory,
+    Raised when a path is neither a directory nor a file,
     nor a symbolic link pointing to them.
     """
 
 
 def main() -> None:
+
     index: dict[str, list[dict[str, str]]] = toml.load('index.toml')
+
     for category, configs in index.items():
+
         cat_dir = Path('src') / category
         if not cat_dir.is_dir():
             continue
+
         for config in configs:
+
             from_ = cat_dir / config['name']
             if not from_.exists():
                 continue
+
             to = Path(config['to']).expanduser()
             to.parent.mkdir(parents=True, exist_ok=True)
-            if to.exists():
-                if not to.is_file() and not to.is_dir():
-                    raise UnsupportedPath(
-                        f'{to} is neither a file nor a directory, '
-                        f'nor a symbolic link pointing to them.'
-                    )
+
+            # `to.is_symlink()` is for the symlink that points to
+            # a non-existing directory or file
+            if to.is_dir() or to.is_file() or to.is_symlink():
                 backup = Path('_backup') / category / config['name']
                 backup.parent.mkdir(parents=True, exist_ok=True)
-                if backup.is_file():
-                    backup.unlink()
-                elif backup.is_dir():
+                if backup.is_dir():
                     shutil.rmtree(backup)
+                elif backup.exists():
+                    backup.unlink()
                 to.rename(backup)
+            elif to.exists():
+                raise UnsupportedPath(
+                    f'{to} is neither a directory nor a file, '
+                    f'nor a symbolic link pointing to them.'
+                )
+
             to.symlink_to(from_.resolve())
 
 
